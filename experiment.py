ab57c9d451e03a1dec82d69330c38ca0563dafea
Janez Starc <janez.starc@ijs.si>
2015-10-01 11:11:24 +0200

    update

--- experiment.py
+++ experiment.py
@@ -42,7 +42,29 @@ def pad_sequences(sequences, maxlen=None, dim=1, dtype='float32',
         else:
             raise ValueError("Padding type '%s' not understood" % padding)
     return x
-    
+
+def get_minibatches_idx(n, minibatch_size, shuffle=False):
+    """
+    Used to shuffle the dataset at each iteration.
+    """
+
+    idx_list = numpy.arange(n, dtype="int32")
+
+    if shuffle:
+        numpy.random.shuffle(idx_list)
+
+    minibatches = []
+    minibatch_start = 0
+    for i in range(n // minibatch_size):
+        minibatches.append(idx_list[minibatch_start:
+                                    minibatch_start + minibatch_size])
+        minibatch_start += minibatch_size
+
+    if (minibatch_start != n):
+        # Make a minibatch out of what is left
+        minibatches.append(idx_list[minibatch_start:])
+
+    return zip(range(len(minibatches)), minibatches)
 
 #train, dev, test = load_data.load_all_snli_datasets('data/snli_1.0/')
 #glove = load_data.import_glove('data/snli_vectors.txt')
