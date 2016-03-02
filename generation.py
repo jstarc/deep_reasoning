3080184c5e1e96e13f3b799e6e50708ae7e74971
Janez Starc <janez.starc@ijs.si>
2016-03-02 18:51:30 +0100

    generation code clean

--- generation.py
+++ generation.py
@@ -10,12 +10,12 @@ from keras.models import Sequential, Graph
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Flatten, Lambda
-from keras.layers.core import Dense, Merge, RepeatVector, TimeDistributedDense, Activation
+from keras.layers.core import Dense, RepeatVector, TimeDistributedDense, Activation
 
 from keras import backend as K
 
  
-from seq2seq.models import Seq2seq, SimpleSeq2seq, AttentionSeq2seq
+from seq2seq.models import AttentionSeq2seq
 import load_data
 import misc
 from hierarchical_softmax import HierarchicalSoftmax
@@ -24,37 +24,7 @@ from keras.utils.generic_utils import Progbar
 PREM_LEN = 22
 HYPO_LEN = 12
 
-def make_model(hidden_size = 10, embed_size = 50, batch_size = 64):
-    
-    batch_input_shape = (batch_size, PREM_LEN, embed_size)
-    
-    model = Sequential()
-    seq2seq = AttentionSeq2seq(
-        batch_input_shape = batch_input_shape,
-        input_dim = embed_size,
-        hidden_dim=embed_size,
-        output_dim=embed_size,
-        output_length=HYPO_LEN,
-        depth=1,
-        bidirectional=False,
-    )
-
-    model.add(seq2seq)
-    model.compile(loss='mse', optimizer='rmsprop', sample_weight_mode="temporal")
-    return model
-    
-def make_adverse_model(generative_model, embed_size = 50, batch_size = 64):
-    discriminator = Sequential()
-    discriminator.add(LSTM(embed_size))
-    discriminator.add(Dense(1))
-    
-    batch_input_shape = (batch_size, PREM_LEN, embed_size)
-    
-    graph = Graph()
-    graph.add_input()
-        
-    
-    
+         
 def make_embed_model(examples ,vocab_size, hidden_size = 10, embed_size = 50, batch_size = 64, hs = True, ci = True):
     
     batch_input_shape = (batch_size, PREM_LEN, embed_size)
@@ -153,52 +123,6 @@ def generation_embded_test(train, glove, model, batch_size = 64):
     return model.train_on_batch(data)
         #p.add(len(X_p),[('train_loss', train_loss)])
     
-    
-def train_model_generation(train, dev, glove, model, model_dir =  'models/curr_model', nb_epochs = 20, batch_size = 64, worse_steps = 5):
-    validation_freq = 1000
-    X_dev_p, X_dev_h, y_dev = load_data.prepare_split_vec_dataset(dev, glove)
-    test_losses = []
-    stats = [['iter', 'train_loss', 'dev_loss']]
-    exit_loop = False
-    embed_size = X_dev_p[0].shape[1]
-
-    if not os.path.exists(model_dir):
-         os.makedirs(model_dir)
-
-    for e in range(nb_epochs):
-        print "Epoch ", e
-        mb = load_data.get_minibatches_idx(len(train), batch_size, shuffle=True)
-        #mb = load_data.get_minibatches_idx_bucketing_both(train,([9,11,13,16,22],[6,7,8,10,13]), batch_size, shuffle=True)
-        p = Progbar(len(train))
-        for i, train_index in mb:
-            if len(train_index) != batch_size:
-                continue
-            X_train_p, X_train_h, _ = load_data.prepare_split_vec_dataset([train[k] for k in train_index], glove)
-            padded_p = load_data.pad_sequences(X_train_p, maxlen = PREM_LEN, dim = embed_size, padding = 'pre')
-            padded_h = load_data.pad_sequences(X_train_h, maxlen = HYPO_LEN, dim = embed_size, padding = 'post')
-            sw = (np.sum(padded_h, axis = 2) != 0).astype(float)
-            train_loss = float(model.train_on_batch(padded_p, padded_h, sample_weight = sw)[0])
-            p.add(len(train_index),[('train_loss', train_loss)])
-            iter = e * len(mb) + i + 1
-            #if iter % validation_freq == 0:
-        sys.stdout.write('\n')
-        dev_loss = validate_model_generation(model, X_dev_p, X_dev_h, glove, batch_size)
-        sys.stdout.write('\n\n')
-        test_losses.append(dev_loss)
-        stats.append([iter,  p.sum_values['train_loss'][0] / p.sum_values['train_loss'][1], dev_loss])
-        if (np.array(test_losses[-worse_steps:]) > min(test_losses)).all():
-            exit_loop = True
-            break
-        else:
-            fn = model_dir + '/model' '~' + str(iter)
-            open(fn + '.json', 'w').write(model.to_json())
-            model.save_weights(fn + '.h5')
-        if exit_loop:
-            break
-    with open(model_dir + '/stats.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(stats)
-        
 def train_model_embed(train, dev, glove, model, nb_epochs = 20, batch_size = 64, worse_steps = 5, hs=True, ci = True):
     X_dev_p, X_dev_h, y_dev = load_data.prepare_split_vec_dataset(dev, glove)
     embed_size = X_dev_p[0].shape[1]
@@ -229,54 +153,29 @@ def train_model_embed(train, dev, glove, model, nb_epochs = 20, batch_size = 64,
             p.add(len(train_index),[('train_loss', train_loss)])
         sys.stdout.write('\n')
 
-def validate_model_generation(model, X_dev_p, X_dev_h, glove, batch_size):
-    glove_mat = np.array(glove.values())
-    glove_mat = glove_mat / np.linalg.norm(glove_mat, axis = 1)[:,None]    
-    dmb = load_data.get_minibatches_idx(len(X_dev_p), batch_size, shuffle=True)
-    p = Progbar(len(X_dev_p))
-    for i, dev_index in dmb:
-        if len(dev_index) != batch_size:
-            continue
-        padded_p = load_data.pad_sequences(X_dev_p[dev_index], maxlen=PREM_LEN, dim = len(X_dev_p[0][0]), padding = 'pre')
-        padded_h = load_data.pad_sequences(X_dev_h[dev_index], maxlen=HYPO_LEN, dim = len(X_dev_p[0][0]), padding = 'post')
-        sw = (np.sum(padded_h, axis = 2) != 0).astype(float) 
-        loss, acc = model.test_on_batch(padded_p, padded_h, accuracy=True,sample_weight = sw)
-        p.add(len(dev_index),[('test_loss',loss)])
-    loss = p.sum_values['test_loss'][0] / p.sum_values['test_loss'][1]
-    return loss
 
-
-def generation_predict(model, glove, premise, batch_size = 64):
-    X_p = load_data.load_word_vecs(premise, glove)
-    X_p = load_data.pad_sequences([X_p], maxlen=PREM_LEN, dim = len(X_p[0]))
-    X = np.zeros((batch_size, X_p.shape[1], X_p.shape[2]))
-    X[0] = X_p[0]
-    model_pred = model.predict_on_batch(X)
-    return model_pred[0][0]
-
-def generation_predict_embed(model, glove, premise, embed_index, class_index,  batch_size = 64):
+def generation_predict_embed(model, glove, premise, embed_index, class_index,  batch_size = 64, hs = True, ci = True):
     X_p = load_data.load_word_vecs(premise, glove)
     X_p = load_data.pad_sequences([X_p], maxlen=PREM_LEN, dim = len(X_p[0]))
     X = np.zeros((batch_size, X_p.shape[1], X_p.shape[2]))
     X[0] = X_p[0]
     E = np.zeros((batch_size, 1))
     E[0][0] = embed_index
-    C = np.zeros((batch_size, 3))
-    C[0][class_index] = 1
-    data = {'premise_input': X, 'embed_input': E, 'class_input':C}
-    hs = True
+    
+    data = {'premise_input': X, 'embed_input': E}
+    
+    if ci:
+      C = np.zeros((batch_size, 3))
+      C[0][class_index] = 1
+      data['class_input'] = C
+    
     if hs:
         data['train_input'] = np.zeros((batch_size, HYPO_LEN))
+    
     model_pred = model.predict_on_batch(data)
     return model_pred['output'][0]
 
-def project(embed_sent, glove, glove_mat):
-    result = []
-    for e in embed_sent:
-        result.append(get_word(e, glove, glove_mat))
-    return result
-
-def project2(embed_sent, glove):
+def project(embed_sent, glove):
     result = []
     ind = np.argmax(embed_sent, axis = 1)
     for i in ind:
@@ -299,14 +198,10 @@ def get_rank(array, word, glove, glove_mat):
     ind = glove.keys().index(word)
     return len(glove) - prod.argsort().argsort()[ind]
 
-
-def softmax(w, t = 1.0):
-    e = np.exp(np.array(w) / t)
-    dist = e / np.sum(e)
-    return dist
-
-#glove_mat = np.array(glove.values())    
-#glove_mat = glove_mat / np.linalg.norm(glove_mat, axis = 1)[:,None]
+def make_glove_mat(glove):
+    glove_mat = np.array(glove.values())    
+    glove_mat = glove_mat / np.linalg.norm(glove_mat, axis = 1)[:,None]
+    return glove_mat
 
 def transform_dataset(dataset, class_str = 'entailment', max_prem_len = sys.maxint, max_hypo_len = sys.maxint):
     uniq = set()
@@ -329,13 +224,7 @@ def add_eol_to_hypo(dataset):
 
 def word_overlap(premise, hypo):
     return len([h for h in hypo if h in premise]) / float(len(hypo))    
-
-def predict_lexicalize(example, model, glove, glove_mat):
-    pred = generation_predict(model, glove, example[0])
-    print " ".join(example[0])
-    print " ".join(example[1])
-    print " ".join(project(pred, glove, glove_mat))           
-
+           
 
 def hypo_to_string(hypo, eos = '.'):
     if eos in hypo:
@@ -345,12 +234,10 @@ def hypo_to_string(hypo, eos = '.'):
         return " ".join(hypo)
  
 
-def test_genmodel(gen_model, classify_model, train, dev, glove, glove_alter):
+def test_genmodel(gen_model, train, dev, glove, classify_model = None, glove_alter = None):
     dev_count = 10
     gens_count = 10
-    glove_mat = np.array(glove.values())
-    glove_mat = glove_mat / np.linalg.norm(glove_mat, axis = 1)[:,None]
-    
+   
     for ex in dev[:dev_count]:
         print " ".join(ex[0])
         print " ".join(ex[1])
@@ -358,11 +245,14 @@ def test_genmodel(gen_model, classify_model, train, dev, glove, glove_alter):
         for train_index in np.random.random_integers(0, len(train), gens_count):
             pred = generation_predict_embed(gen_model, glove, ex[0], train_index)
             
-            #gen_lex = project(pred, glove, glove_mat)
-            gen_lex = project2(pred, glove)
+            gen_lex = project(pred, glove)
             gen_str = hypo_to_string(gen_lex)
-            probs = misc.predict_example(" ".join(ex[0]), gen_str, classify_model, glove_alter)
-            print probs[0][0][2], gen_str
+            
+            if classify_model != None and glove_alter != None: 
+                probs = misc.predict_example(" ".join(ex[0]), gen_str, classify_model, glove_alter)
+                print probs[0][0][2], gen_str
+            else:
+                print gen_str
         print
     
 if __name__ == "__main__":
