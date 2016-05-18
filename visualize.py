aebf71f0859412247e1e6c267346865b33587f13
Janez Starc <janez.starc@ijs.si>
2016-05-18 07:16:29 +0000

    test specific points

--- visualize.py
+++ visualize.py
@@ -1,6 +1,19 @@
 import numpy as np
 import load_data
 from generative_alg import *
+from keras.utils.generic_utils import Progbar
+
+def test_points(premises, labels, noises, gtest, cmodel, hypo_len):
+    p = Progbar(len(premises))
+    hypos = []
+    for i in range(len(labels) / 16):
+        words, _  = generative_predict_beam(gtest, premises[i * 16: (i+1)*16], noises[i * 16: (i+1)*16,None,:], labels[i * 16: (i+1)*16], True, hypo_len)
+        hypos.append(words)
+        p.add(len(words))
+    hypos = np.vstack(hypos)
+    cpreds = cmodel.evaluate([premises, hypos], labels)
+    print cpreds
+
 
 def print_hypos(premise, label, gen_test, beam_size, hypo_len, noise_size, wi):
     words = single_generate(premise, label, gen_test, beam_size, hypo_len, noise_size)
